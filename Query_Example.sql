1d1f788e5896ff1fcace48d0e4f88d8a027d6fc7
Luca Vasapolli <vasapolliluca@gmail.com>
2018-04-12 23:34:04 +0200

    Popolare_il_db

---
+++ Query_Example.sql
@@ -0,0 +1,7 @@
+INSERT INTO `superprof`.`users` (`user_id`, `uname`, `surname`, `birth`, `gender`, `email`, `passw`, `utype`) VALUES ('1', 'Albert', 'Einstein', '1879-03-14', 'M', 'alberteinstein@gmail.com', 'emc2', '1');
+INSERT INTO `superprof`.`users` (`user_id`, `uname`, `surname`, `birth`, `gender`, `email`, `passw`, `utype`) VALUES ('2', 'Enrico', 'Fermi', '1901-10-29', 'M', 'enricofermi@gmail.com', 'nuclear', '1');
+INSERT INTO `superprof`.`users` (`user_id`, `uname`, `surname`, `birth`, `gender`, `email`, `passw`, `utype`) VALUES ('3', 'Richard', 'Feynman', '1918-05-11', 'M', 'feynman@gmail.com', 'QED', '1');
+INSERT INTO `superprof`.`users` (`user_id`, `uname`, `surname`, `birth`, `gender`, `email`, `passw`, `utype`) VALUES ('4', 'Erminio', 'Greco', '1985-07-18', 'M', 'erminio@gmail.com', 'Ilovetheteam', '2');
+INSERT INTO `superprof`.`prof` (`prof_id`, `subjects`) VALUES ('1', 'Math');
+INSERT INTO `superprof`.`prof` (`prof_id`, `subjects`) VALUES ('2', 'Fisisca');
+INSERT INTO `superprof`.`prof` (`prof_id`, `subjects`) VALUES ('3', 'Tutto');
